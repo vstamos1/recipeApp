0a5da0a0488420b95e235013b2d4be28553cb780
Vincent Stamos <vincentstamos@Vincents-MacBook-Pro-2.local>
2019-05-22 12:05:46 -0400

    set up router and links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+import Home from './pages/Home';
+import SingleRecipe from './pages/SingleRecipe';
+import Recipes from './pages/Recipes';
+import Default from './pages/Default';
+
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <div className="App">
-      hello from recipe app
-      <h2 className="text-slanted orangeText">hello</h2>
-    </div>
+    <Router>
+    <main>
+    <Navbar></Navbar>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/recipes" exact component={Recipes}></Route>
+        <Route path="/recipes/:id" component={SingleRecipe}></Route>
+        <Route  component={Default}></Route>
+      </Switch>
+      </main>
+    </Router>
   );
 }
 
